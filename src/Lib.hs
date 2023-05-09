25d0ccfeee72d22b9c92b1d0d26d7d99a36b822a
Tomasz Brengos <tomasz@genlambda.com>
2023-05-09 21:46:35 +0200

    Add someFunc example for parsing

--- src/Lib.hs
+++ src/Lib.hs
@@ -5,4 +5,9 @@ import Parser
 import Semantics.CEK
 
 someFunc :: IO ()
-someFunc = pure () 
+someFunc = do
+  let input = "(define (factorial n) (if (= n 0) 1 (* n (factorial (- n 1)))))"
+  putStrLn $ "Input: " ++ input
+  case readExpr input of
+    Left err -> putStrLn $ "Error: " ++ show err
+    Right val -> putStrLn $ "Parsed: " ++ show val
